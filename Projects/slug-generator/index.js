b4b9ac455799f3372bcb7b61cb9b786e2a95feb4
Xola Qika <qikaxola@gmail.com>
2026-02-16 18:22:10 +0200

    Update index.js

--- Projects/slug-generator/index.js
+++ Projects/slug-generator/index.js
@@ -1,1 +1,13 @@
+let input = document.getElementById("inputt");
+let button1 = document.getElementById("buttonn");
+let button2 = document.getElementById("buttonn1");
 
+let message = ""
+
+
+button1.onclick = function() {
+    if(input.value.length > 0 && input.value.includes(" "))
+        message = input.value.replace(/\s+/g, '-').toLowerCase();
+
+    buttonn1.value += message;
+}
