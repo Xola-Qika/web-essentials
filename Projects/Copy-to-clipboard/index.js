894b55e145c1950b5d503dd1a2d0efda71e9f9f8
Xola Qika <qikaxola@gmail.com>
2026-02-16 18:17:58 +0200

    Update index.js

--- Projects/Copy-to-clipboard/index.js
+++ Projects/Copy-to-clipboard/index.js
@@ -1,1 +1,8 @@
+let input = document.getElementById("box1");
+let btn = document.getElementById("box2");
+let box3 = document.getElementById("box3");
 
+btn.addEventListener("click", function () {
+    input.select();
+    navigator.clipboard.writeText(input.value);
+});
